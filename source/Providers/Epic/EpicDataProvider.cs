b99ca8c1bf3ccb91e790d4190613915384f17863
Justin Delano <jddelano056@gmail.com>
2026-02-15 16:48:36 -0500

    feat(epic): add Legendary plugin to IsCapable check

---
+++ source/Providers/Epic/EpicDataProvider.cs
@@ -0,0 +1,83 @@
+using PlayniteAchievements.Models;
+using PlayniteAchievements.Providers;
+using Playnite.SDK;
+using Playnite.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlayniteAchievements.Providers.Epic
+{
+    public sealed class EpicDataProvider : IDataProvider, IDisposable
+    {
+        private readonly EpicSessionManager _sessionManager;
+        private readonly EpicScanner _scanner;
+        private readonly HttpClient _httpClient;
+
+        private static readonly Guid EpicPluginId = ResolveEpicPluginId();
+        internal static readonly Guid LegendaryPluginId = Guid.Parse("EAD65C3B-2F8F-4E37-B4E6-B3DE6BE540C6");
+
+        public EpicDataProvider(
+            ILogger logger,
+            PlayniteAchievementsSettings settings,
+            IPlayniteAPI playniteApi,
+            EpicSessionManager sessionManager)
+        {
+            if (logger == null) throw new ArgumentNullException(nameof(logger));
+            if (settings == null) throw new ArgumentNullException(nameof(settings));
+            if (playniteApi == null) throw new ArgumentNullException(nameof(playniteApi));
+            if (sessionManager == null) throw new ArgumentNullException(nameof(sessionManager));
+
+            _httpClient = new HttpClient();
+            _sessionManager = sessionManager;
+
+            var apiClient = new EpicApiClient(_httpClient, logger, sessionManager);
+            _scanner = new EpicScanner(settings, apiClient, sessionManager, logger);
+
+            _ = _sessionManager.PrimeAuthenticationStateAsync(CancellationToken.None);
+        }
+
+        public string ProviderName => ResourceProvider.GetString("LOCPlayAch_Provider_Epic");
+
+        public string ProviderIconKey => "ProviderIconEpic";
+
+        public string ProviderColorHex => "#26BBFF";
+
+        public bool IsAuthenticated => _sessionManager.IsAuthenticated;
+
+        public bool IsCapable(Game game) => IsEpicCapable(game);
+
+        public static bool IsEpicCapable(Game game)
+        {
+            return game != null && (game.PluginId == EpicPluginId || game.PluginId == LegendaryPluginId);
+        }
+
+        public Task<RebuildPayload> ScanAsync(
+            List<Game> gamesToScan,
+            Action<ProviderScanUpdate> progressCallback,
+            Func<GameAchievementData, Task> onGameScanned,
+            CancellationToken cancel)
+        {
+            return _scanner.ScanAsync(gamesToScan, progressCallback, onGameScanned, cancel);
+        }
+
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+        }
+
+        private static Guid ResolveEpicPluginId()
+        {
+            try
+            {
+                return BuiltinExtensions.GetIdFromExtension(BuiltinExtension.EpicLibrary);
+            }
+            catch
+            {
+                return Guid.Empty;
+            }
+        }
+    }
+}
