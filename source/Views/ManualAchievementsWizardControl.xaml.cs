f209e8bd609250e37d1226137c424fa7d8dcd318
Justin Delano <jddelano056@gmail.com>
2026-02-28 15:31:30 -0500

    refactor(manual): inline search/edit UI into wizard control
    Move all search and edit stage UI directly into
    ManualAchievementsWizardControl.xaml. Add event handlers for
    search keyboard shortcuts and double-click navigation.

--- source/Views/ManualAchievementsWizardControl.xaml.cs
+++ source/Views/ManualAchievementsWizardControl.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using PlayniteAchievements.ViewModels;
 
 namespace PlayniteAchievements.Views
@@ -28,17 +29,16 @@ namespace PlayniteAchievements.Views
         {
             Loaded -= OnLoaded;
 
-            if (_viewModel.SearchVm != null &&
-                !_viewModel.IsEditingStage &&
-                !string.IsNullOrWhiteSpace(_viewModel.SearchVm.SearchText))
+            if (!_viewModel.IsEditingStage &&
+                !string.IsNullOrWhiteSpace(_viewModel.SearchText))
             {
                 try
                 {
-                    await _viewModel.SearchVm.SearchAsync();
+                    await _viewModel.ExecuteSearchAsync();
                 }
                 catch
                 {
-                    // Error is handled by SearchViewModel
+                    // Error is handled by ViewModel
                 }
             }
         }
@@ -46,6 +46,31 @@ namespace PlayniteAchievements.Views
         private void ViewModel_RequestClose(object sender, EventArgs e)
         {
             // Handled by parent window
+        }
+
+        private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && _viewModel?.SearchCommand.CanExecute(null) == true)
+            {
+                _viewModel.SearchCommand.Execute(null);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                if (_viewModel?.CancelCommand.CanExecute(null) == true)
+                {
+                    _viewModel.CancelCommand.Execute(null);
+                }
+                e.Handled = true;
+            }
+        }
+
+        private void SearchDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (_viewModel?.SelectedResult != null && _viewModel.NextCommand.CanExecute(null))
+            {
+                _viewModel.NextCommand.Execute(null);
+            }
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
