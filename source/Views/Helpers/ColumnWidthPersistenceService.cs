d124783b15df93d9c2a4e9e590ac22fc44566f0c
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-03-12 08:06:55 -0400

    Revert "fix(columns): use proportional rescaling for all window resizes"
    This reverts commit 28db57ed8e4e32e9dfbd8034ae63eac4f18282c8.

--- source/Views/Helpers/ColumnWidthPersistenceService.cs
+++ source/Views/Helpers/ColumnWidthPersistenceService.cs
@@ -241,7 +241,7 @@ namespace PlayniteAchievements.Views.Helpers
             }
 
             var isVisibilityActivation = e.PreviousSize.Width <= 1;
-            var shouldRescaleAll = !isVisibilityActivation;
+            var shouldRescaleAll = _shouldRescaleAllOnInitialLoad || !isVisibilityActivation;
 
             _grid.Dispatcher.BeginInvoke(
                 new Action(() =>
@@ -249,6 +249,10 @@ namespace PlayniteAchievements.Views.Helpers
                     if (_grid.IsLoaded && !_isResizeInProgress)
                     {
                         NormalizeColumnsToContainer(shouldRescaleAll);
+                        if (shouldRescaleAll && _shouldRescaleAllOnInitialLoad && IsValidWidth(GetGridAvailableWidth()))
+                        {
+                            _shouldRescaleAllOnInitialLoad = false;
+                        }
                     }
                 }),
                 DispatcherPriority.Render);
