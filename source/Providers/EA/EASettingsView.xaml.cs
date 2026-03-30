d82edb236ae3f7ea8998686c36c820261aa95ac3
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-03-30 19:50:54 -0400

    feat(ea): add EA settings view with login/logout UI

---
+++ source/Providers/EA/EASettingsView.xaml.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Playnite.SDK;
+using PlayniteAchievements.Models;
+using PlayniteAchievements.Providers.Settings;
+using PlayniteAchievements.Services;
+using PlayniteAchievements.Services.Logging;
+
+namespace PlayniteAchievements.Providers.EA
+{
+    public partial class EASettingsView : ProviderSettingsViewBase, IAuthRefreshable
+    {
+        private static readonly ILogger Logger = PluginLogger.GetLogger(nameof(EASettingsView));
+        private readonly EASessionManager _sessionManager;
+        private EASettings _eaSettings;
+
+        #region DependencyProperties
+
+        public static readonly DependencyProperty AuthBusyProperty =
+            DependencyProperty.Register(nameof(AuthBusy), typeof(bool), typeof(EASettingsView), new PropertyMetadata(false));
+
+        public bool AuthBusy
+        {
+            get => (bool)GetValue(AuthBusyProperty);
+            set => SetValue(AuthBusyProperty, value);
+        }
+
+        public static readonly DependencyProperty IsAuthenticatedProperty =
+            DependencyProperty.Register(nameof(IsAuthenticated), typeof(bool), typeof(EASettingsView), new PropertyMetadata(false));
+
+        public bool IsAuthenticated
+        {
+            get => (bool)GetValue(IsAuthenticatedProperty);
+            set => SetValue(IsAuthenticatedProperty, value);
+        }
+
+        public static readonly DependencyProperty AuthStatusProperty =
+            DependencyProperty.Register(nameof(AuthStatus), typeof(string), typeof(EASettingsView), new PropertyMetadata(string.Empty));
+
+        public string AuthStatus
+        {
+            get => (string)GetValue(AuthStatusProperty);
+            set => SetValue(AuthStatusProperty, value);
+        }
+
+        #endregion
+
+        public new EASettings Settings => _eaSettings;
+
+        public EASettingsView(EASessionManager sessionManager)
+        {
+            _sessionManager = sessionManager ?? throw new ArgumentNullException(nameof(sessionManager));
+            InitializeComponent();
+        }
+
+        public override void Initialize(IProviderSettings settings)
+        {
+            _eaSettings = settings as EASettings;
+            base.Initialize(settings);
+            _ = RefreshAuthStatusAsync();
+        }
+
+        private void UpdateAuthStatus(AuthProbeResult result)
+        {
+            var isAuthenticated = result?.IsSuccess ?? false;
+            IsAuthenticated = isAuthenticated;
+            var providerName = ResourceProvider.GetString("LOCPlayAch_Provider_EA");
+
+            if (isAuthenticated)
+            {
+                AuthStatus = string.Format(
+                    ResourceProvider.GetString("LOCPlayAch_Settings_Auth_AlreadyAuthenticated"),
+                    providerName);
+            }
+            else
+            {
+                var localized = !string.IsNullOrWhiteSpace(result?.MessageKey)
+                    ? ResourceProvider.GetString(result.MessageKey)
+                    : null;
+
+                AuthStatus = string.IsNullOrWhiteSpace(localized) || string.Equals(localized, result?.MessageKey, StringComparison.Ordinal)
+                    ? string.Format(
+                        ResourceProvider.GetString("LOCPlayAch_Settings_Auth_NotAuthenticated"),
+                        providerName)
+                    : localized;
+            }
+        }
+
+        public async Task RefreshAuthStatusAsync()
+        {
+            AuthProbeResult result;
+            try
+            {
+                result = await _sessionManager.ProbeAuthStateAsync(CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Logger.Debug(ex, "EA auth probe failed during settings refresh.");
+                result = AuthProbeResult.ProbeFailed();
+            }
+
+            UpdateAuthStatus(result);
+        }
+
+        private async void LoginWeb_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SetAuthBusy(true);
+                var result = await _sessionManager.AuthenticateInteractiveAsync(forceInteractive: true, CancellationToken.None);
+                if (result.IsSuccess)
+                {
+                    await RefreshAuthStatusAsync();
+                    PlayniteAchievementsPlugin.NotifySettingsSaved();
+                }
+                else
+                {
+                    UpdateAuthStatus(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "EA web login failed");
+            }
+            finally
+            {
+                SetAuthBusy(false);
+            }
+        }
+
+        private async void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SetAuthBusy(true);
+                _sessionManager.ClearSession();
+                await RefreshAuthStatusAsync();
+                PlayniteAchievementsPlugin.NotifySettingsSaved();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "EA logout failed");
+            }
+            finally
+            {
+                SetAuthBusy(false);
+            }
+        }
+
+        private void SetAuthBusy(bool busy)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                AuthBusy = busy;
+            }
+            else
+            {
+                Dispatcher.BeginInvoke(new Action(() => AuthBusy = busy));
+            }
+        }
+    }
+}
