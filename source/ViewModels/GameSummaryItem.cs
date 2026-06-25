36ee0f30b8a7c211017c86667c590ac5bdf6d901
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-06-25 18:45:54 -0400

    fix: alias ObservableObject to disambiguate after generic import

--- source/ViewModels/GameSummaryItem.cs
+++ source/ViewModels/GameSummaryItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using PlayniteAchievements.Common;
 using PlayniteAchievements.Models.Achievements;
 using PlayniteAchievements.Services;
+using ObservableObject = PlayniteAchievements.Common.ObservableObject;
 
 namespace PlayniteAchievements.ViewModels
 {
