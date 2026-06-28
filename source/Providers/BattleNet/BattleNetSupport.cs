098c230f4be3243c287f69bbfc2d7b6f9e5bb58d
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-06-28 13:33:34 -0400

    feat: enable SC2 achievement support in BattleNet provider

--- source/Providers/BattleNet/BattleNetSupport.cs
+++ source/Providers/BattleNet/BattleNetSupport.cs
@@ -7,8 +7,7 @@ namespace PlayniteAchievements.Providers.BattleNet
     internal static class BattleNetGameSupport
     {
         public static readonly Guid BattleNetPluginId = Guid.Parse("E3C26A3D-D695-4CB7-A769-5FF7612C7EDD");
-        // Temporarily disabled until SC2 support is validated.
-        internal const bool IsSc2Enabled = false;
+        internal const bool IsSc2Enabled = true;
 
         public static bool IsSupported(Game game, BattleNetSettings settings)
         {
