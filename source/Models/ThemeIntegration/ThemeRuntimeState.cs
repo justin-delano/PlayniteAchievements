f689019be12abb4c24573d79962ce5e05b5960ff
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-07-16 20:55:59 -0400

    feat: register category summaries view state on theme runtime state

--- source/Models/ThemeIntegration/ThemeRuntimeState.cs
+++ source/Models/ThemeIntegration/ThemeRuntimeState.cs
@@ -5,6 +5,7 @@ namespace PlayniteAchievements.Models.ThemeIntegration
         public SelectedGameRuntimeState SelectedGame { get; set; } = SelectedGameRuntimeState.Empty;
         public LibraryRuntimeState Library { get; set; } = new LibraryRuntimeState();
         public SelectedGameAchievementViewState SelectedGameAchievements { get; } = new SelectedGameAchievementViewState();
+        public CategorySummaryViewState CategorySummaries { get; } = new CategorySummaryViewState();
         public LibraryAchievementViewState LibraryAchievements { get; } = new LibraryAchievementViewState();
         public GameSummaryViewState GameSummaries { get; } = new GameSummaryViewState();
         public FriendRuntimeState Friends { get; set; } = FriendRuntimeState.Empty;
