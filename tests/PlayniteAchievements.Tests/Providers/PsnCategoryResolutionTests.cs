67684f998d780c534d8992bdc479b7df81e91ebc
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-07-23 18:20:33 -0400

    test: cover PSN trophy group category resolution
    Add PsnCategoryResolutionTests for ResolveCategory (base->null, named DLC, blank/missing->null) and MapTrophyGroupToCategoryType base/DLC classification; wire the helper and its deps into the test project.

---
+++ tests/PlayniteAchievements.Tests/Providers/PsnCategoryResolutionTests.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlayniteAchievements.Providers.PSN;
+
+namespace PlayniteAchievements.Tests.Providers
+{
+    [TestClass]
+    public class PsnCategoryResolutionTests
+    {
+        private static IReadOnlyDictionary<string, string> Groups()
+        {
+            return new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                ["default"] = "Base Game",
+                ["001"] = "Frozen Wilds",
+                ["002"] = "   ",
+            };
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("   ")]
+        [DataRow("default")]
+        [DataRow("base")]
+        [DataRow("000")]
+        public void ResolveCategory_BaseGroup_ReturnsNull(string groupId)
+        {
+            Assert.IsNull(PsnTrophyCategoryHelper.ResolveCategory(groupId, Groups()));
+        }
+
+        [TestMethod]
+        public void ResolveCategory_DlcGroupWithName_ReturnsTrimmedName()
+        {
+            Assert.AreEqual("Frozen Wilds", PsnTrophyCategoryHelper.ResolveCategory("001", Groups()));
+        }
+
+        [TestMethod]
+        public void ResolveCategory_DlcGroupWithBlankName_ReturnsNull()
+        {
+            Assert.IsNull(PsnTrophyCategoryHelper.ResolveCategory("002", Groups()));
+        }
+
+        [TestMethod]
+        public void ResolveCategory_DlcGroupMissingFromMap_ReturnsNull()
+        {
+            Assert.IsNull(PsnTrophyCategoryHelper.ResolveCategory("999", Groups()));
+        }
+
+        [TestMethod]
+        public void ResolveCategory_NullMap_ReturnsNull()
+        {
+            Assert.IsNull(PsnTrophyCategoryHelper.ResolveCategory("001", null));
+        }
+
+        [DataTestMethod]
+        [DataRow(null, "Base")]
+        [DataRow("", "Base")]
+        [DataRow("default", "Base")]
+        [DataRow("base", "Base")]
+        [DataRow("000", "Base")]
+        [DataRow("001", "DLC")]
+        [DataRow("anything-else", "DLC")]
+        public void MapTrophyGroupToCategoryType_ClassifiesBaseVsDlc(string groupId, string expected)
+        {
+            Assert.AreEqual(expected, PsnTrophyCategoryHelper.MapTrophyGroupToCategoryType(groupId));
+        }
+    }
+}
