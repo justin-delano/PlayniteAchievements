a636cf7aa365992215afba090d9f465e86ad6454
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-08-13 22:17:16 -0400

    feat(showcase): localize game list sort and mosaic source names

--- source/Views/Showcase/ShowcaseUiText.cs
+++ source/Views/Showcase/ShowcaseUiText.cs
@@ -47,6 +47,12 @@ namespace PlayniteAchievements.Views.Showcase
         public static string ScreenshotVariantName(ShowcaseScreenshotVariant value) =>
             EnumValueName("LOCPlayAch_Showcase_ScreenshotVariant_", value);
 
+        public static string GameListSortName(ShowcaseGameListSort value) =>
+            EnumValueName("LOCPlayAch_Showcase_GameListSort_", value);
+
+        public static string GameMosaicSourceName(ShowcaseGameMosaicSource value) =>
+            EnumValueName("LOCPlayAch_Showcase_GameMosaicSource_", value);
+
         public static string FitModeName(ShowcaseImageFitMode value) =>
             EnumValueName("LOCPlayAch_Showcase_ImageFit_", value);
 
