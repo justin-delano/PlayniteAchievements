bcf4a5f8d8f57db04e545877e057809f70fe4ecc
Justin Delano <25873234+justin-delano@users.noreply.github.com>
2026-08-25 23:25:58 -0400

    refactor(settings): route the rarity selector through MultiSelectMenu

--- source/Views/Settings/Controls/RaritySelectorMenu.cs
+++ source/Views/Settings/Controls/RaritySelectorMenu.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using Playnite.SDK;
 using PlayniteAchievements.Models.Achievements;
 
@@ -150,43 +148,12 @@ namespace PlayniteAchievements.Views.Settings.Controls
 
         private static MenuItem CreateMenuItem(Button button, string header, bool isChecked, Action<bool> onToggle)
         {
-            var item = new MenuItem
-            {
-                Header = header,
-                IsCheckable = true,
-                StaysOpenOnClick = true,
-                IsChecked = isChecked
-            };
-
-            if (button?.TryFindResource("AchievementMultiSelectMenuItemStyle") is Style itemStyle)
-            {
-                item.Style = itemStyle;
-            }
-
-            item.Click += (_, __) => onToggle?.Invoke(item.IsChecked);
-            return item;
+            return MultiSelectMenu.CreateItem(button, header, isChecked, onToggle);
         }
 
         private static void OpenContextMenu(Button button, ContextMenu menu)
         {
-            if (button == null || menu == null || menu.Items.Count == 0)
-            {
-                return;
-            }
-
-            RoutedEventHandler onClosed = null;
-            onClosed = (_, __) =>
-            {
-                menu.Closed -= onClosed;
-                button.ReleaseMouseCapture();
-            };
-
-            menu.Closed += onClosed;
-            menu.PlacementTarget = button;
-            menu.Placement = PlacementMode.Bottom;
-            menu.HorizontalOffset = 0;
-            menu.VerticalOffset = 0;
-            menu.IsOpen = true;
+            MultiSelectMenu.Open(button, menu);
         }
 
         private static string Localize(string key)
